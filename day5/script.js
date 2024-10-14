cb7bd61ed47846c013930ba96e39222c223f413e
tiwariaman6565 <at344054@gmail.com>
2024-10-14 10:29:57 +0530

    javascript

--- day5/script.js
+++ day5/script.js
@@ -27,16 +27,40 @@
 // let a;
 // let b=null;
 // console.log(typeof b)
-function validate(){
-    let un=document.getElementById("un").value;
-    let pass=document.getElementById("pass").value;
-    if(un=="admin" && pass=="admin"){
-       document.getElementById("res").innerHTML="Login success";
-    } 
-    else{
-        document.getElementById("res").innerHTML="Invalid username or password";
-    }
+// function validate(){
+//     let un=document.getElementById("un").value;
+//     let pass=document.getElementById("pass").value;
+//     if(un=="admin" && pass=="admin"){
+//        document.getElementById("res").innerHTML="Login success";
+//     } 
+//     else{
+//         document.getElementById("res").innerHTML="Invalid username or password";
+//     }
 
     
     
+// }
+// let x=Number("12");
+// let str='123';
+// console.log(x+str);
+
+// console.log(typeof x);
+// "use strict"
+// num=12;
+// console.log(num);
+// let a=()=>{
+//     console.log("hello world");
+// };
+// a();
+// let a=(x,y)=>{
+//     console.log(x+y);
+// };
+// a(12,34);
+
+// let say=()=> console.log("hello");
+// say();
+function display(){
+    let p1=document.querySelector("p");
+    p1.innerHTML="Result";
+    p1.style.backgroundColor="red";
 }
