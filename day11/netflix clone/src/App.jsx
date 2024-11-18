8b3108f5cace7c26b978131732b762ff36a20a15
tiwariaman6565 <at344054@gmail.com>
2024-11-18 10:28:34 +0530

    netflixclone

---
+++ day11/netflix clone/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import MoviesList from "./MoviesList.jsx";
+
+const App = () => {
+    return (
+        <div>
+        <h1 style={{backgroundColor:"black",color:"white",textAlign:"center"}}>Netflix Clone App</h1>
+        <MoviesList />
+        </div>
+    )
+}
+export default App
---
+++ day11/netflix clone/src/movies.js
@@ -0,0 +1,30 @@
+const movies=[
+    {
+       id:1,
+       image:"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/4/f/r/2009-dvd-eagle-hindi-tarzan-the-wonder-car-original-imahfdpjpqg85j5d.jpeg?q=70",
+        title:"TARZAN THE WONDER CAR",
+        language:"Hindi",
+        cost:249,
+    },
+    {
+        id:2,
+        image:"https://rukminim2.flixcart.com/image/612/612/k3dc7m80/movie/f/q/n/2018-blu-ray-universal-pictures-excel-home-videos-english-original-imafmggqtjc6uxwz.jpeg?q=70",
+        title:"JURASSIC WORLD",
+        language: "English",
+        cost: 299,
+    },
+    {
+      id: 3,
+      image:"https://rukminim2.flixcart.com/image/612/612/movie/t/g/d/sultan-2016-hindi-dvd-yashraj-original-imaeht9h4g7skwpz.jpeg?q=70",
+        title:"SULTAN",
+        language: "Hindi",
+        cost: 199,
+    },
+    {
+        id:4,
+        image:"https://rukminim2.flixcart.com/image/612/612/av-media/movies/q/w/c/gangs-of-wasseypur-i-original-imadnzhqzxzrgumh.jpeg?q=70",
+        title:"GANGS OF WASSEYPUR",
+        language: "Hindi",
+        cost: 259,
+    }];
+export default movies;
