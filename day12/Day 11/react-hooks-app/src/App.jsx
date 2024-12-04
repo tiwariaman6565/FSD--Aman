5594d351677ec7df348f7cdd033c6ce7846d7f9b
tiwariaman6565 <at344054@gmail.com>
2024-12-04 15:29:28 +0530

    th

--- day12/Day 11/react-hooks-app/src/App.jsx
+++ day12/Day 11/react-hooks-app/src/App.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-// import CounterApp from "./components/CounterApp.jsx";
+import CounterApp from "./components/CounterApp.jsx";
 import ImageApp from "./components/ImageApp.jsx";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import MainLayout from "./components/MainLayout.jsx";
 const App = () => {
-    return( <div>
-        {/*<CounterApp />*/}
-        <ImageApp />
-    </div>
+    return(
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<MainLayout/>}/>
+                    <Route path={"/counterapp"} element={<CounterApp/>}/>
+                    <Route path={"/imageapp"} element={<ImageApp/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
     );
 };
-export default App;
+export default 
+App;
--- day12/Day 11/react-hooks-app/src/components/MainLayout.jsx
+++ day12/Day 11/react-hooks-app/src/components/MainLayout.jsx
@@ -1,4 +1,20 @@
 import React from "react";
+import {Link,Outlet} from "react-router-dom";
 const MainLayout=()=>{
-    
+    return(
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to={"/counterapp"}>CounterApp</Link>
+                    </li>
+                    <li>
+                        <Link to={"/imageapp"}>ImageApp</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Outlet/>
+        </div>
+    )
 }
+export default MainLayout;
